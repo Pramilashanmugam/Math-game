0f5b2274f9c53761727eb4ad64007f00a0294074
Pramila shanmugam <pramila.sl@gmail.com>
2024-01-26 22:19:02 +0000

    incrementscore and incrementwrong answer function created

--- assets/js/script.js
+++ assets/js/script.js
@@ -78,11 +78,13 @@ function calculateCorrectAnswer() {
 }
 
 function incrementScore() {
-
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
 }
 
 function incrementWrongAnswer() {
-
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
 }
 
 function displayAdditionQuestion(operand1, operand2) {
